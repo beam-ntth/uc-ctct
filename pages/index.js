19a4ce89d29863d610669824cf8326eb466cd2e2
beam-ntth <beam4318.t@gmail.com>
2022-01-27 09:28:55 -0800

    fixed uc seal and added Outlook login

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,6 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
-  const [btn, useBtn] = useState([styles.circle1, styles.circle2, styles.circle3])
-  let logoImage = <img className={styles.fadeInAndOut} src='/asset/images/ucd-logo.png' alt='UC Davis Logo' style={{height: 'auto', width: '100%'}} />
-  if (btn[0] == styles.circle2) {
-    logoImage = <img key='logo1' className={styles.fadeInAndOut} src="/asset/images/ucsf-logo.png" alt='UC Davis Logo' style={{height: 'auto', width: '75%'}} />
-  } else if (btn[0] == styles.circle3) {
-    logoImage = <img key='logo2' className={styles.fadeInAndOut} src="/asset/images/ucla-logo.png" alt='UC Davis Logo' style={{height: 'auto', width: '100%'}} />
-  } else {
-    logoImage = <img key='logo3' className={styles.fadeInAndOut} src='/asset/images/ucd-logo.png' alt='UC Davis Logo' style={{height: 'auto', width: '100%'}} />
-  }
-
   return (
     <div className={styles.container}>
       <Head>
@@ -26,16 +16,8 @@ export default function Home() {
         {/* Logo Side */}
         <div className={styles.logo}>
           <div className={styles.subLogo}>
-            <div className={styles.showImages}>
-              {logoImage}
-            </div>
+            <img key='logo1' className={styles.fadeInAndOut} src="/asset/images/uc-seal.png" alt='UC Davis Logo' style={{height: 'auto', width: '30%', marginBottom: '5%'}} />
             <h1 className={styles.logoTitle}>Welcome to UC Coordination Tools</h1>
-            <p className={styles.logoSubTitle}>Click to see all organizations</p>
-            <div className={styles.carosel}>
-              <div className={btn[0]} onClick={() => useBtn([styles.circle1, styles.circle2, styles.circle3])}></div>
-              <div className={btn[1]} onClick={() => useBtn([styles.circle2, styles.circle1, styles.circle3])}></div>
-              <div className={btn[2]} onClick={() => useBtn([styles.circle3, styles.circle2, styles.circle1])}></div>
-            </div>
           </div>
         </div>
         {/* Login Side */}
@@ -47,6 +29,10 @@ export default function Home() {
             <a className={styles.signinBtn} href="/api/auth/google">
               <img style={{height: '60%', width: 'auto', paddingRight: '1rem'}} src='/asset/images/google-logo.png' alt='Google Logo'/>
               Sign in with Google
+            </a>
+            <a className={styles.signinBtn} href="/api/auth/google">
+              <img style={{height: '60%', width: 'auto', paddingRight: '1rem'}} src='/asset/images/outlook-logo.png' alt='Google Logo'/>
+              Sign in with Outlook
             </a>
           </div>
         </div>
