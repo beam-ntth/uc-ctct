744e6768cd7ba1a90f29fee99f80dc5af9157cb1
jtmeneses707 <jtmenese@gmail.com>
2022-01-26 18:33:42 -0800

    Changes to file structure

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    CLIENT_ID: '304769669090-jbm2d80nfpp83j1bgkvrj80cpr0qv7oq.apps.googleusercontent.com',
-    CLIENT_SECRET: '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',
-    LOCAL_URL: 'http://localhost:3000'
-  },
-}
