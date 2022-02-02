9c71032fa829ea2505c6e814e6612ba42deade52
emmyhuynh <emmy.huynh8@gmail.com>
2022-02-02 10:35:55 -0800

    added side bar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from '../styles/navbar.css'
+
+import { Nav } from 'react-bootstrap';
+
 import UCSealBlue from '/asset/images/uc_seal_blue.png';
 
 
@@ -9,9 +12,17 @@ export default function Navbar() {
             {/* Logo Side */}
             <div className={styles.logo}>
                 <div className={styles.subLogo}>
-                    <img src={UCSealBlue} alt="UC Seal"/>;
+                    <img src={UCSealBlue} alt="UC Seal" />;
                 </div>
             </div>
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/home">Active</Nav.Link>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+                <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                </Nav.Link>
+            </Nav>
         </div>
     )
 }
