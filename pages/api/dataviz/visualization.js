8b998bec21a8a9e28562a7ac42c3a82c539fcaf1
emmyhuynh <emmy.huynh8@gmail.com>
2022-02-08 11:07:48 -0800

    did one row of data analytics

--- pages/api/dataviz/visualization.js
+++ pages/api/dataviz/visualization.js
@@ -1,16 +1,51 @@
-// export default function handler(req, res) {
-//     console.log(req.query)
-//     const clinicName = req.query.name;
-//     if (clinicName === 'clinic1') {
-//     res.status(200).json([
-//       {
-//         trainingType: 'FNP',
-//         affiliation: 'UCSF',
-//         region: 'Coastal',
-//         status: 'Cold Called'
-//       }]);
-//       return;
+export default function handler(req, res) {
+  const query = req.query;
 
-//     // clinic name, training type, affiliation, region, status
+  // api/dataviz/visualization
 
-// }
+  // clinic name, training type, affiliation, region, status
+  const clinic1 = {
+    id: 'clinic1',
+    name: 'Clinic 1',
+    lastUpdated: '01/21/2019',
+    status: 0,
+  }
+
+  const clinic2 = {
+    id: 'clinic2',
+    name: 'Clinic 2',
+    lastUpdated: '01/22/2019',
+    status: 2,
+  }
+
+  const clinic3 = {
+    id: 'clinic4',
+    name: 'Clinic 4',
+    lastUpdated: '01/23/2019',
+    status: 1,
+  }
+
+  const clinic4 = {
+    id: 'clinic5',
+    name: 'Clinic 5',
+    lastUpdated: '02/23/2019',
+    status: 3,
+  }
+
+  if (req.query.location == 'site1') {
+    res.status(200).json([clinic1, clinic3, clinic2, clinic4])
+    return;
+  }
+
+  if (req.query.location == 'site2') {
+    res.status(200).json([clinic4, clinic2])
+    return;
+  }
+
+  if (req.query.location == 'site3') {
+    res.status(200).json([clinic1])
+    return;
+  }
+
+  res.status(400).json("400: Bad Request");
+}
--- pages/sites/dataviz/index.js
+++ pages/sites/dataviz/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../../../styles/Visualization.module.css'
 import Link from 'next/link'
 
+import React, { useEffect } from "react";
 import Navbar from '../../../components/shared/navbar/navbar';
 import Header from '../../../components/shared/header/header';
 
@@ -11,29 +12,64 @@ import Header from '../../../components/shared/header/header';
 //     return { props: { data } }
 //   }
 
-export default function Visualization() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Data Analytics</title>
-                <meta name="description" content="University of California - Clinic Coordination Tools" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={styles.main}>
-                <Navbar icons={[false, true, false, false, false]} />
-                <div className={styles.content}>
-                    <Header header="Data Analytics" date="Today: Febuary 2, 2022" imgSrc="/asset/images/user-image.png" />
-                    <div className={styles.data}>
-                        <div className={styles.row}>
-                            <p className='col1DV' style={{marginLeft: '2rem'}}>Clinic Name</p>
-                            <p className='col2DV'>Training Type</p>
-                            <p className='col3DV'>Affiliation</p>
-                            <p className='col4DV'>Region</p>
-                            <p className='col5DV'>Status</p>
+export default function Visualization({ data }) {
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <Head>
+          <title>Data Analytics</title>
+          <meta name="description" content="University of California - Clinic Coordination Tools" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <Navbar icons={[false, true, false, false, false]} />
+          <div className={styles.content}>
+            <Header header="Data Analytics" date="Today: Febuary 2, 2022" imgSrc="/asset/images/user-image.png" />
+            <div className={styles.data}>
+              <div className={styles.row}>
+                <p className='col1DV'>Clinic Name</p>
+                <p className='col2DV'>Training Type</p>
+                <p className='col3DV'>Affiliation</p>
+                <p className='col4DV'>Region</p>
+                <p className='col5DV'>Status</p>
+              </div>
+              {/* { */}
+                {/* // data.map((x, ind) => {
+                  // let statusText = 'N/A'
+
+                  // if (x['status'] === 0) {
+                  //   statusText = 'Need To Contact'
+                  // }
+                  // if (x['status'] === 1) {
+                  //   statusText = 'Need To Follow Up'
+                  // }
+                  // if (x['status'] === 2) {
+                  //   statusText = 'Contacted'
+                  // }
+                  // if (x['status'] === 3) {
+                  //   statusText = 'Connected'
+                  // } */}
+
+                  {/* return ( */}
+                    {/* <Link href={`/sites/database/clinics/clinic?name=${x['id']}`}> */}
+                      <div className="displayRow">
+                        <div className="rowContentClinics">
+                          <p className="col1DV" style={{ marginLeft: '2rem' }}>{'Clinic 1'}</p>
+                          <p className="col2DV" >{'FNP'}</p>
+                          <p className="col3DV">{'UCSF'}</p>
+                          <p className="col4DV">{'Southern'}</p>
+                          <p className="col5DV">{'Status'}</p>
                         </div>
-                    </div>
-                </div>
-            </main>
-        </div>
-    )
+                        {/* <div className={`tag${x['status']}`}></div> */}
+                      </div>
+                    {/* </Link> */}
+                  {/* ) */}
+                {/* }) */}
+              {/* } */}
+            </div>
+          </div>
+        </main>
+      </div>
+    </React.Fragment>
+  )
 }
