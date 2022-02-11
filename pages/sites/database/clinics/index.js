928b80bc3c7c25c1d8d78af03f0fda3c8e4625ad
Joshua-Troy Meneses <jtmenese@gmail.com>
2022-02-10 21:27:12 -0800

    Fixed issue with item.read() - Needed to use location as both id and partition key

--- pages/sites/database/clinics/index.js
+++ pages/sites/database/clinics/index.js
@@ -13,16 +13,19 @@ import { CosmosClient } from '@azure/cosmos';
 // Setting up access to API
 const endpoint = process.env.COSMOS_ENDPOINT;
 const key = process.env.COSMOS_KEY;
-const client = new CosmosClient({endpoint , key});
+const client = new CosmosClient({ endpoint, key });
 
 export async function getServerSideProps(context) {
-  const location = context.query.location
+  const location = context.query.location;
+  console.log("Location is", location);
   const database = client.database("uc-ctct");
   const site_container = database.container("Sites");
   const clinic_container = database.container("Clinics");
   const { resources: data } = await clinic_container.items.query(`SELECT * from c WHERE c.site_id = '${location}'`).fetchAll();
-  const { resources: note_data } = await site_container.items.query(`SELECT * from c WHERE c.id = '${location}'`).fetchAll();
-  console.log(note_data)
+  console.log("Clinics data is", data);
+  // const { resources: note_data } = await site_container.items.query(`SELECT * from c WHERE c.id = '${location}'`).fetchAll();
+  const { resource: note_data } = await site_container.item(location, location).read();
+  console.log("Note data is", note_data);
   return { props: { data, note_data } }
 }
 
@@ -48,23 +51,23 @@ export default function Clinics({ data, note_data }) {
           <div className={styles.content}>
             <Header header="Management Overview - Site 1" imgSrc="/asset/images/user-image.png" back={router.back} />
             <div className={styles.data}>
-              <div style={{width: '90%', display: 'flex', flexDirection: 'column', paddingTop: '2rem'}}>
-                <div style={{width: '100%', display: 'flex', marginBottom: '2rem'}}>
-                  <p className="titleClinics" style={{width: '80%', paddingLeft: '2rem', margin: 0, display: 'flex', alignItems: 'center'}}>Site Notes</p>
-                  <div style={{width: '20%', display: 'flex', justifyContent: 'center'}}>
+              <div style={{ width: '90%', display: 'flex', flexDirection: 'column', paddingTop: '2rem' }}>
+                <div style={{ width: '100%', display: 'flex', marginBottom: '2rem' }}>
+                  <p className="titleClinics" style={{ width: '80%', paddingLeft: '2rem', margin: 0, display: 'flex', alignItems: 'center' }}>Site Notes</p>
+                  <div style={{ width: '20%', display: 'flex', justifyContent: 'center' }}>
                     <div className='editButton' onClick={() => setOpenNote(true)}>+ Add Note</div>
                   </div>
                 </div>
                 {
-                    note_data[0].notes.map((x, ind) => {
-                      return (<Accordion x={x} ind={ind} />)
-                    })
-                  }
+                  note_data.notes.map((x, ind) => {
+                    return (<Accordion x={x} ind={ind} />)
+                  })
+                }
               </div>
             </div>
             <div className={styles.data}>
               <div className={styles.row}>
-                <p className="row1Clinics" style={{marginLeft: '2rem'}}>Clinic Name</p>
+                <p className="row1Clinics" style={{ marginLeft: '2rem' }}>Clinic Name</p>
                 <p className="row2Clinics">Last Updated</p>
                 <p className="row3Clinics">Status</p>
               </div>
--- pages/sites/visual/index.js
+++ pages/sites/visual/index.js
@@ -8,12 +8,25 @@ import Navbar from '../../../components/shared/navbar/navbar';
 import Header from '../../../components/shared/header/header';
 import PieChart from '../../../components/Charts/piechart';
 import BarChart from '../../../components/Charts/barcharts';
+import { CosmosClient } from '@azure/cosmos';
+
+const endpoint = process.env.COSMOS_ENDPOINT;
+const key = process.env.COSMOS_KEY;
+const client = new CosmosClient({ endpoint, key });
 
 export async function getServerSideProps() {
+  // console.log("CREATING CLIENT");
+  // const database = client.database({ id: "Test Database" });
+  const { database } = await client.databases.createIfNotExists({ id: "Test DB" });
+  console.log(database.id);
+  // const container = await database.container.createIfNotExists({ id: "Test Container 1" });
+  const { container } = await database.containers.createIfNotExists({ id: "Test Container 1" });
+  console.log("Creating", container);
+  // console.log(database);
   const res = await fetch(`${process.env.LOCAL_URL}/api/clinic`)
   const data = await res.json()
   console.log(data);
- return { props: { data } }
+  return { props: { data } }
 }
 
 export default function Visualization({ data }) {
@@ -37,20 +50,20 @@ export default function Visualization({ data }) {
                 <p className='col4DV'>Region</p>
                 <p className='col5DV'>Status</p>
               </div>
-              
+
               {data.map((x, ind) => {
                 return (
-          
-                    <div className='displayRow' key={`elem_${ind}`}>
-                        <p className="clinicCol1">{x.name}</p>
-                        <p className="clinicCol2">{x.training_type}</p>
-                        <p className="clinicCol3">{x.affiliation}</p>
-                        <p className="clinicCol4">{x.region}</p>
-                        <p className="clinicCol4">{x.status}</p>
-                        </div>
-                      )
-                    })
-                  }
+
+                  <div className='displayRow' key={`elem_${ind}`}>
+                    <p className="clinicCol1">{x.name}</p>
+                    <p className="clinicCol2">{x.training_type}</p>
+                    <p className="clinicCol3">{x.affiliation}</p>
+                    <p className="clinicCol4">{x.region}</p>
+                    <p className="clinicCol4">{x.status}</p>
+                  </div>
+                )
+              })
+              }
 
               {/* return ( */}
               {/* <Link href={`/sites/database/clinics/clinic?name=${x['id']}`}> */}
@@ -60,7 +73,7 @@ export default function Visualization({ data }) {
                   <p className="col2DV" >{'FNP'}</p>
                   <p className="col3DV">{'UCSF'}</p>
                   <p className="col4DV">{'Southern'}</p>
-                  <p className="col5DV"style={{ marginRight: '2rem' }}>{'Status'}</p>
+                  <p className="col5DV" style={{ marginRight: '2rem' }}>{'Status'}</p>
                 </div>
                 {/* <div className={`tag${x['status']}`}></div> */}
               </div>
@@ -70,23 +83,23 @@ export default function Visualization({ data }) {
               {/* } */}
             </div>
             <div className={styles.mainCharts}>
-                <div className={styles.chart}>
-                  <div className={styles.chartTitle}>
-                    <p>Clinic Regions</p>
-                  </div>
-                  <div style={{ height: '90%', width: 'auto' }}>
-                    <PieChart />
-                  </div>
+              <div className={styles.chart}>
+                <div className={styles.chartTitle}>
+                  <p>Clinic Regions</p>
                 </div>
-                <div className={styles.chart}>
-                  <div className={styles.chartTitle}>
-                    <p>Training Types</p>
-                  </div>
-                  <div style={{ height: '90%', width: 'auto' }}>
-                    <PieChart />
-                  </div>
+                <div style={{ height: '90%', width: 'auto' }}>
+                  <PieChart />
                 </div>
               </div>
+              <div className={styles.chart}>
+                <div className={styles.chartTitle}>
+                  <p>Training Types</p>
+                </div>
+                <div style={{ height: '90%', width: 'auto' }}>
+                  <PieChart />
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
