4571c587afd5b25b2ccc4b7f0150d9b2f02c89ef
Joshua-Troy Meneses <jtmenese@gmail.com>
2022-02-11 13:50:46 -0800

    Separated NProgress setup from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,11 @@
 import '../styles/globals.css';
 import Router from 'next/router'
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css'
-// import ../style
-// Progress bar set up 
-NProgress.configure({
-  minimum: 0.4,
-  easing: 'ease',
-  speed: 800,
-  showSpinner: false,
-});
+import NProgress from '../components/NProgress/nprogressSetup';
 
+// Setting up NProgress to work with onChange for routes. 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
