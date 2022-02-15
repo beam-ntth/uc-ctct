158f7508498587b13e4a96b37e10939d6fb7a44a
beam-ntth <beam4318.t@gmail.com>
2022-02-15 12:34:46 -0800

    added debug comment

--- api-lib/azure/azureConfig.js
+++ api-lib/azure/azureConfig.js
@@ -3,4 +3,5 @@ import { CosmosClient } from '@azure/cosmos';
 // Setting up connection with the DB.
 const endpoint = process.env.COSMOS_ENDPOINT;
 const key = process.env.COSMOS_KEY;
+console.log(endpoint)
 export const client = new CosmosClient({ endpoint, key });
