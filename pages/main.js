b14e80e5980de60ea63df124f9113ee46f0cd092
m-tilve <97307487+m-tilve@users.noreply.github.com>
2022-02-22 16:02:49 -0800

    Improved appearance of dashboard map

--- pages/main.js
+++ pages/main.js
@@ -67,8 +67,9 @@ export default function Main() {
           <Header header="Welcome, Rosalind De Lisser!" imgSrc="/asset/images/user-image.png" />
           <div className={styles.activities}>
             <div className={styles.activityBox}>
-              <h1 className={styles.actTitle}>Map of Clinics/Students</h1>
-              <h4 className={styles.legend}><img src='/asset/images/clinic-pin.png' /> is Clinic and <img src='/asset/images/student-pin.png' /> is Student</h4>
+              <h1 className={styles.actTitle}>Map of Clinics and Students</h1>
+              <h4 className={styles.legend}><img src='/asset/images/clinic-pin.png' /> Clinic</h4> 
+              <h4 className={styles.legend}><img src='/asset/images/student-pin.png' />  Student</h4>
               <div className={styles.mapFrame}>
                 <div className={styles.mapContainer}>
                   <GoogleMapReact bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY }} defaultCenter={center} defaultZoom={zoom} >
