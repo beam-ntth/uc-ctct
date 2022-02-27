304ffd2564fdf8a9cba47af79afe93aa62769de1
beam-ntth <beam4318.t@gmail.com>
2022-02-27 17:01:37 +0700

    map markers hoverable

---
+++ components/shared/marker/marker.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export default function Marker(props) {
+    const [hover, setHover] = useState(false)
+    return (
+        <React.Fragment>
+            <div className='map-container'>
+                {
+                    props.type == 'clinic' ? 
+                    <img height={hover ? '35px' : '30px'} width="auto" src="/asset/images/clinic-pin.png"
+                    onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} />
+                    :
+                    <img height={hover ? '35px' : '30px'} width="auto" src="/asset/images/student-pin.png"
+                    onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} />
+                }
+                {hover ? <div className='map-text'>
+                    <p>{props.data}</p>
+                </div> : null}
+            </div>
+            <style jsx>
+                {
+                    `
+                        .map-container {
+                            display: flex;
+                            flex-direction: row;
+                            height: 100px;
+                            width: 200px;
+                        }
+
+                        .map-text {
+                            width: 100%;
+                            padding: 10px;
+                            background-color: #fff;
+                            border-radius: 10px;
+                        }
+                    `
+                }
+            </style>
+        </React.Fragment>
+    )
+}
