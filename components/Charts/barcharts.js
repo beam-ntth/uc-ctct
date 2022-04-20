89d0d5dbd14b9fa30dc17f0c7df95c38c8b26f0d
m-tilve <97307487+m-tilve@users.noreply.github.com>
2022-04-19 21:29:01 -0700

    Added actual data to the graphs

--- components/Charts/barcharts.js
+++ components/Charts/barcharts.js
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
+import { getAllStudents } from '../../api-lib/azure/azureOps';
 
-const state = {
-  labels: ['UCD', 'UCLA', 'UCSF',
-           'UCI'],
-  datasets: [
-    {
-      label: 'Number of  active students',
-      boxWidth: 2,
-      backgroundColor:['RGB(0, 142, 170)' , 'RGB(241, 138, 0)','RGB(61, 174, 43)','RGB(118, 8, 130)'],
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 0,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
 
 export default function BarChart() {
-    return (
-      <React.Fragment>
-        <Bar data={state} options={{
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-          legend: {
-            boxWidth: 10,
-            display:false,
-            position:'right',
-            pointStyle: "circle",
-          }
+
+  const [data, setData] = useState([0,0,0,0])
+
+  const state = {
+    labels: ['UCD', 'UCLA', 'UCSF',
+            'UCI'],
+    datasets: [
+      {
+        label: 'Number of  active students',
+        boxWidth: 2,
+        backgroundColor:['RGB(0, 142, 170)' , 'RGB(241, 138, 0)','RGB(61, 174, 43)','RGB(118, 8, 130)'],
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 0,
+        data: data
+      }
+    ]
+  }
+
+  const loadData = async() => {
+    const response = await getAllStudents();
+    const splitData = [0, 0, 0, 0]
+    response.forEach(x => {
+      if (x.status == "Active") {
+        if (x.location_affiliation == "UC Davis") {
+          splitData[0]++
+        } else if (x.location_affiliation == "UC Los Angeles") {
+          splitData[1]++
+        } else if (x.location_affiliation == "UC San Francisco") {
+          splitData[2]++
+        } else {
+          splitData[3]++
         }
-        }}
-        />
-      </React.Fragment>
-    );
+      }
+      return
+    })
+    setData(splitData)
+  }
+
+  useEffect(() => loadData(), [])
+
+  return (
+    <React.Fragment>
+      <Bar data={state} options={{
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+        legend: {
+          boxWidth: 10,
+          display:false,
+          position:'right',
+          pointStyle: "circle",
+        }
+      }
+      }}
+      />
+    </React.Fragment>
+  );
 }
--- components/Charts/linechart.js
+++ components/Charts/linechart.js
@@ -1,6 +1,9 @@
-import React from 'react';
 import { Bar } from 'react-chartjs-2';
+import React, { useEffect, useState } from 'react';
+import { getAllStudents } from '../../api-lib/azure/azureOps';
 
+export default function LineChart() {
+  const [data, setData] = useState([0,0,0,0])
 
 const state = {
   labels: ['UCD', 'UCLA', 'UCSF',
@@ -11,13 +14,34 @@ const state = {
       backgroundColor:['RGB(0, 142, 170)' , 'RGB(241, 138, 0)','RGB(61, 174, 43)','RGB(118, 8, 130)'],
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 0,
-      data: [65, 59, 80, 81, 56],
+      data: data,
       barThickness:5
     },
   ]
 }
+    const loadData = async() => {
+      const response = await getAllStudents();
+      const splitData = [0, 0, 0, 0]
+      response.forEach(x => {
+        if (x.assignment.isAssigned) {
+          if (x.location_affiliation == "UC Davis") {
+            splitData[0]++
+          } else if (x.location_affiliation == "UC Los Angeles") {
+            splitData[1]++
+          } else if (x.location_affiliation == "UC San Francisco") {
+            splitData[2]++
+          } else {
+            splitData[3]++
+          }
+        }
+        return
+      })
+      setData(splitData)
+    }
 
-export default function LineChart() {
+    useEffect(() => loadData(), [])
+
+
     return (
       <React.Fragment>
         <Bar data={state} options={{
